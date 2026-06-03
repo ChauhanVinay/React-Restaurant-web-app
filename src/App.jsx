e7e95332284dd6760336159f0b7e939fb92237c7
ChauhanVinay <vc0627344@gmail.com>
2026-06-03 08:12:52 +0530

    Adding feature of Cart open and close

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header.jsx";
 import Meals from "./components/Meals/Meals.jsx";
 import Cart from "./components/Cart/Cart.jsx";
 
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
 
+   const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+   };
+   
   return (
     <Fragment>
-    
-    <Cart />
-      <Header />
+
+     {cartIsShown && <Cart onClose={hideCartHandler}/> }
+      <Header  onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
