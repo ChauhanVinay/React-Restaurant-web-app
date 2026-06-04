0688f41f790811977d6937935e9a569aca822398
ChauhanVinay <vc0627344@gmail.com>
2026-06-04 07:37:01 +0530

    Sjhowing and enabling of Cart Items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import { useContext } from 'react';
+
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
-  // Hardcoded items 
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-{[
-{ id: 'c1', name: 'Sushi', amount: 2, price: 22.99 },
-{ id: 'c2', name: 'Barbecue Burger', amount: 1, price: 12.99 }
-      ].map((item) => (
-        <li key={item.id} className={classes['cart-item-row']}>
-          <div>
-            <h2>{item.name}</h2>
-            <div className={classes.summary}>
-              <span className={classes.price}>${item.price.toFixed(2)}</span>
-              <span className={classes.amount}>x {item.amount}</span>
-            </div>
-          </div>
-        </li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -28,13 +39,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>$58.97</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import classes from "./Input.module.css";
+import React from 'react';
+import classes from './Input.module.css';
 
-const Input = (props) => {
-    return (
-      <div className={classes.input}>
-        <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input}/>
-      </div>
-    );
-};
+// Wrap the component function with forwardRef
+const Input = React.forwardRef((props, ref) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      {/* Pass the forwarded ref directly down to the 
+        native HTML input element 
+      */}
+      <input ref={ref} {...props.input} />
+    </div>
+  );
+});
 
 export default Input;
